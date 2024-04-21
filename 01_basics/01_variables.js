1987975cbbd2a5d013a05c299e8a5387adba6ec4
ownerchintu <94180994+ownerchintu@users.noreply.github.com>
2024-04-21 10:58:22 +0000

    learned variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId=144553
+let accountEmail= "aforabishek122@gmail.com"
+var accountPassword= "abhi"
+accountCity= "haldwani"
+
+//accountId=2
+accountEmail="kdada@gmail.com"
+accountPassword="2ee"
+accountCity="jaipur" 
+
+console.log(accountId);
+console.table([accountEmail, accountPassword, accountCity])
