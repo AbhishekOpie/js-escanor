e7ffdb980e2e10d51dabf635bff04d9647b0d87a
ownerchintu <94180994+ownerchintu@users.noreply.github.com>
2024-04-22 10:33:27 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,21 @@
+let score = "33abc"
+
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+let = isLoggedIn = 0
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn)
+console.log(typeof booleanIsLoggedIn)
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
