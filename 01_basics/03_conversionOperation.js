4eccc6edf358815a661ec94b5db6bc65c364a231
ownerchintu <94180994+ownerchintu@users.noreply.github.com>
2024-04-23 14:07:57 +0000

    conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,11 +1,11 @@
-let score = "33abc"
+/*let score = "33abc"
 
 console.log(typeof score);
 
-let valueInNumber = Number(score)
+let valueIngameCounterber = gameCounterber(score)
 
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+console.log(typeof valueIngameCounterber);
+console.log(valueIngameCounterber);
 
 let = isLoggedIn = 0
 
@@ -14,8 +14,40 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 console.log(booleanIsLoggedIn)
 console.log(typeof booleanIsLoggedIn)
 
-let someNumber = 33
+let somegameCounterber = 33
 
-let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+let stringgameCounterber = String(somegameCounterber)
+console.log(stringgameCounterber)
+console.log(typeof stringgameCounterber) */
+
+// ************************** OPERATIONS **************************
+
+let value=3
+let negValue = -value
+console.log(negValue)
+//console.log(typeof negValue)
+
+let str1="abhi"
+let str2="shek"
+let str3=str1+str2
+console.log(str3)
+
+let gameCounter1="2"
+let gameCounter2=3
+let isgameCounterber = gameCounterber(gameCounter1)
+console.log(gameCounter2 + isgameCounterber)
+
+console.log("1"+2)
+console.log(1+"2")
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "2")
+
+
+console.log( (3 + 4) * 5 % 3)
+
+
+console.log(+true)
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter)
