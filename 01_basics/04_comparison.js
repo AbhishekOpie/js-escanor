3f67e7f3abd8cb0fcd47dc6b06a1eb1504a1e2ad
ownerchintu <94180994+ownerchintu@users.noreply.github.com>
2024-04-24 13:18:46 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,28 @@
+console.log( 2 > 1)
+console.log( 2 >= 1)
+console.log (2 != 1)
+
+
+console.log("2" > 1) 
+console.log("2" > 1)
+
+
+console.log(null > 0)
+console.log(null == 0)
+console.log(null >= 0)
+
+
+console.log(undefined == 0)
+console.log(undefined >= 0)
+console.log(undefined <= 0)
+
+
+
+// === means strict check , it also checks data type
+
+console.log("2" === 1)
+
+
+
+
+
