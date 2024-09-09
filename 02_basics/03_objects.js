5affb7f7137eff5411f111e0d183bae8a8d6e5cc
ABHISHEK KUMAR <94180994+AbhishekOpie@users.noreply.github.com>
2024-09-09 17:56:40 +0000

    objects

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,10 @@
+const object1 = {
+    name: "abhishek",
+    age: 21,
+    location: "haldwani",
+    email: "aforabhishek@gmail.com",
+    isLoggedIn: false,
+    lastLoginDays: ["monday", "saturday"],
+}
+
+console.log(object1.email)
